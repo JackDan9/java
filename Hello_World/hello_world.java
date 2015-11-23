b96c94a6a4102e700959f32e05f778e51063dbab
RayDanjunjun <1835812864@qq.com>
2015-11-24 00:45:16 +0800

    Add first procedure by java

---
+++ Hello_World/hello_world.java
@@ -0,0 +1,12 @@
+/**
+	@Author:RayDanjunjun
+	@Date: 2015-11-23
+	@Content: Output String of "hello_world"
+*/
+public class MyFirstApp 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello_world");
+	}
+}
