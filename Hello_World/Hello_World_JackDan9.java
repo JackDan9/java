a40095ed8739ba43e9791800ee991e116a0b1c2c
JackDan9 <1835812864@qq.com>
2017-07-12 20:44:07 +0800

    jackdan9 Rewrite

---
+++ Hello_World/Hello_World_JackDan9.java
@@ -0,0 +1,8 @@
+/**
+ * Created by JackDan9 on 2017/7/12.
+ */
+public class Hello_World_JackDan9 {
+    public static void  main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
