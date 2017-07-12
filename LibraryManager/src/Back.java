db701347331ecd1bcd1dddcbb2d96d64306ce43c
JackDan9 <1835812864@qq.com>
2017-07-12 22:45:51 +0800

    jackdan9 Library manager

---
+++ LibraryManager/src/Back.java
@@ -0,0 +1,14 @@
+//import javax.swing.*;
+
+import java.awt.*;
+
+/**
+ * Created by JackDan9 on 2017/7/12.
+ */
+public class Back extends Frame{
+    private static final long serialVersionUID = 1993050319950917L;
+    Label bookidlb = new Label("图书编号"), readeridlb = new Label("读者编号");
+    TextField bookidtxt = new TextField(), readeridtxt = new TextField();
+    Button querybtn = new Button("查询"), borrowbtn = new Button("还书"),closebtn = new Button("清除");
+
+}
