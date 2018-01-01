e02bcf7c45ad5cf7560ef6d140fa359b7f98fabb
JackDan9 <1835812864@qq.com>
2018-01-01 22:49:06 +0800

    :cn: JackDan9 : Java : FrameWork : Library

--- LibraryManager/src/Back.java
+++ LibraryManager/src/Back.java
@@ -11,4 +11,21 @@ public class Back extends Frame{
     TextField bookidtxt = new TextField(), readeridtxt = new TextField();
     Button querybtn = new Button("查询"), borrowbtn = new Button("还书"),closebtn = new Button("清除");
 
+    String SeqLine = "----------------------------------------------";
+    String[] seq = {"图书信息", "读者信息", "借阅信息"};
+    Label[] seqlabel = new Label[3];
+
+    String[] optionname = { "书名：", "作者：", "出版社：", "出版时间：", "定价：", "存量：", "姓名：","类型：", "可借数：", "可借天数：", "借阅日期：","阅读天数：","还书日期" };
+    Label[] alloption = new Label[13];
+    Label[] showoption = new Label[13];
+
+    public Back() {
+        setTitle("图书归还");
+        setLayout(null);
+        setSize(500, 470);
+        setResizable(false);
+        this.setForeground(Color.black);
+        bookidlb.setBounds(50,50,50,20);
+        bookidtxt.setBounds(110, 50, 100, 20);
+    }
 }
